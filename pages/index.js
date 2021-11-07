1aacd1e0be3f2c058a171b7a4fd38eab7470dd37
Rafael Oviedo <oviedo.rp@gmail.com>
2021-11-06 23:08:12 -0300

    update(index.js): adding authentication to app

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import tw from "tailwind-styled-components"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Map from "./components/Map"
 import Link from "next/link"
+import { auth } from "../firebase"
+import { onAuthStateChanged, signOut } from "firebase/auth"
+import { useRouter } from "next/router"
 
 export default function Home() {
+
+  const [user, setUser] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, user => {
+      if (user) {
+        setUser({
+          name: user.displayName,
+          photoUrl: user.photoUrl,
+        }) 
+      } else {
+          setUser(null)
+          router.push("/login")
+        }
+    })
+  })
 
   return (
     <Wrapper >
@@ -14,10 +34,8 @@ export default function Home() {
         <Header>
           <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
           <Profile>
-            <Name>
-              Name name
-            </Name>
-            <UserImage  src="https://media-exp1.licdn.com/dms/image/C4E03AQGdYhue0Z59MQ/profile-displayphoto-shrink_800_800/0/1610410918761?e=1641427200&v=beta&t=DSn78a_4zL-WAWAD0O1xyznriYQXmrxuJa7dKchYEUQ"/>
+            <Name>{user && user.name}</Name>
+            <UserImage src={user && user.photoUrl} onClick={() => signOut(auth)}/>
           </Profile>
         </Header>
         <ActionButtons>
@@ -76,7 +94,13 @@ const Name = tw.div`
 `
 
 const UserImage = tw.img`
-  h-12 w-12 rounded-full border border-gray-200 p-px
+  h-12 
+  w-12 
+  rounded-full 
+  border 
+  border-gray-200 
+  p-px 
+  cursor-pointer
 `
 
 const ActionButtons = tw.div`
