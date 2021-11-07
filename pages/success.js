58fd09b5c18bea1094dc3ee0afb1d2583e812856
Rafael Oviedo <oviedo.rp@gmail.com>
2021-11-07 08:51:47 -0300

    update(success.js): adding success message, image, and styling

--- pages/success.js
+++ pages/success.js
@@ -5,6 +5,12 @@ import Link from "next/link"
 const Success = () => {
   return (
     <Wrapper>
+      <CongratsContainer>
+        <CongratsMessage>
+          Your ride has been succesfully submitted!
+        </CongratsMessage>
+        <CheckImage src="https://www.pngall.com/wp-content/uploads/5/Green-Checklist.png" />
+      </CongratsContainer>
       <ConfirmButtonContainer>
         <Link href="/">
         <ConfirmButton>Back to home</ConfirmButton>
@@ -16,7 +22,30 @@ const Success = () => {
 
 export default Success
 
-const Wrapper = tw.div``
+const Wrapper = tw.div`
+flex flex-col h-screen
+`
+
+const CongratsContainer = tw.div`
+flex-1 
+flex 
+flex-col 
+h-1/2
+`
+
+const CongratsMessage = tw.div`
+bg-gray-200
+flex-1 
+flex 
+flex-col 
+h-1/2
+text-center
+text-3xl
+my-4
+mx-4
+py-64
+font-bold
+`
 
 const ConfirmButton = tw.div`
 bg-black 
@@ -31,4 +60,12 @@ cursor-pointer
 
 const ConfirmButtonContainer = tw.div`
 border-t-2
+`
+
+const CheckImage = tw.img`
+object-contain
+w-full
+rounded-full
+w-20
+h-20
 `
