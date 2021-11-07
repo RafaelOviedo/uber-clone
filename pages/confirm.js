3da852bb60c37b02783b04e2b4830269efd9a8e1
Rafael Oviedo <oviedo.rp@gmail.com>
2021-11-06 23:19:58 -0300

    update(confirm.js): capitalizing component

--- pages/confirm.js
+++ pages/confirm.js
@@ -5,7 +5,7 @@ import {useRouter} from "next/router"
 import RideSelector from "./components/RideSelector"
 import Link from "next/link"
 
-const confirm = () => {
+const Confirm = () => {
   const router = useRouter()
   const { pickup, dropoff } = router.query
   const [pickupCoordinates, setPickupCoordinates] = useState([0, 0])
@@ -67,7 +67,7 @@ const confirm = () => {
     )
 }
 
-export default confirm;
+export default Confirm;
 
 const BackButton = tw.img`
 bg-white
